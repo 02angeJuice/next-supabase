8a99096f540afe3fdfb0cd32d3d770ab0e2769ca
Watcharaphong Sayprang <ws.watcharaphong@gmail.com>
2024-06-27 02:34:00 +0000

    env

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -11,21 +11,25 @@ export default function Login({
 }) {
   const signIn = async (formData: FormData) => {
     "use server";
-
-    const email = formData.get("email") as string;
-    const password = formData.get("password") as string;
     const supabase = createClient();
 
-    const { error } = await supabase.auth.signInWithPassword({
-      email,
-      password,
-    });
+    // console.log(formData.getAll(""))
 
-    if (error) {
-      return redirect("/login?message=Could not authenticate user");
-    }
+    // const email = formData.get("email") as string;
+    // const password = formData.get("password") as string;
+    // const supabase = createClient();
 
-    return redirect("/protected");
+
+    // const { error } = await supabase.auth.signInWithPassword({
+    //   email,
+    //   password,
+    // });
+
+    // if (error) {
+    //   return redirect("/login?message=Could not authenticate user");
+    // }
+
+    // return redirect("/protected");
   };
 
   const signUp = async (formData: FormData) => {
